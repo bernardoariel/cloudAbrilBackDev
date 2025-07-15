3e690d0d3bad50164925f5c38d24a9ef01618c8c
Ariel Bernardo <arielbernardo@hotmail.com>
2025-07-15 20:03:39 -0300

    Agregando gitignore

---
+++ firebase-config-template.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+// console.log("Firebase initialized:", app); // Verifica que Firebase esté correctamente inicializado
+export const auth = getAuth(app);
